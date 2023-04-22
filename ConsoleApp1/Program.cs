8cb683a131ba3e966085b55daecdef707b2e4a46
maxai <maxai@MAX>
2023-04-22 13:31:42 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[5];
+            students[0] = new Student("Maxaiette", "Cubero", "N/A", "BSIT", 4, 24, "Jovito", "Maunate");
+            students[1] = new Student("Maxaiette", "Cubero", "N/A", "BSIT", 4, 24, "Jovito", "Maunate");
+            students[2] = new Student("Maxaiette", "Cubero", "N/A", "BSIT", 4, 24, "Jovito", "Maunate");
+            students[3] = new Student("Maxaiette", "Cubero", "N/A", "BSIT", 4, 24, "Jovito", "Maunate");
+            students[4] = new Student("Maxaiette", "Cubero", "N/A", "BSIT", 4, 24, "Jovito", "Maunate");
+
+            foreach (Student student in students) 
+            {
+                student.intro();
+            }
+            
+                
+            
+        }
+
+           
+    }
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     public class Student
+    {
+       public string firstName { get; set; }
+      public  string lastName { get; set; }
+      public  string middleName { get; set; }
+      public  string course { get; set; }
+      public int grade { get; set; }
+      public  int age { get; set; }
+      public  string fatherName { get; set; }
+      public  string motherName { get; set; }
+        public Student(string firstName, string lastName, 
+            string middleName, string course, 
+            int grade, int age, string 
+            fatherName, string motherName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.middleName = middleName;
+            this.course = course;
+            this.grade = grade;
+            this.age = age;
+            this.fatherName = fatherName;
+            this.motherName = motherName;
+        }
+        public void intro()
+        {
+            Console.WriteLine(firstName+ " " + lastName + " " + middleName + " " + course + " " + grade + " " + age + " " + fatherName + " " + motherName   );
+        }
+
+   
+
+      
+    }
+}
